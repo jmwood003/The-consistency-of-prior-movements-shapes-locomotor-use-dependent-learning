a9818f9c761fb11daf3963a6535cea84c210a291
jmwood003 <47223948+jmwood003@users.noreply.github.com>
2020-05-19 08:09:46 -0400

    More edits

--- Code/ConfusionMatrix.m
+++ Code/ConfusionMatrix.m
@@ -12,7 +12,7 @@ Tmu = 22;
 Conditions = {'All', 'Stable', 'Variable', 'Uniform'};
 
 %Number of simulations per condition
-N = 100;
+N = 1000;
 
 %Change Seed
 rng('shuffle','twister');
@@ -62,7 +62,6 @@ for i = 1:N*3
 end
 
 %Create Final CMs for each condition
-<<<<<<< Updated upstream
 FM(1).all = round(100*CMall/sum(CMall(1,:)))/100;
 %Stable
 CMs = [sum(CMbys(Sidx,:)); sum(CM2p(Sidx,:))];
@@ -73,7 +72,6 @@ FM(3).all = round(100*CMv/sum(CMv(1,:)))/100;
 %Uniform
 CMu = [sum(CMbys(Uidx,:)); sum(CM2p(Uidx,:))];
 FM(4).all = round(100*CMu/sum(CMu(1,:)))/100;
-=======
 FM(1).all = round(100*CMall/sum(CMall(1,:)),3)/100;
 %Stable
 CMs = [sum(CMbys(Sidx,:)); sum(CM2p(Sidx,:))];
@@ -84,7 +82,6 @@ FM(3).all = round(100*CMv/sum(CMv(1,:)),3)/100;
 %Uniform
 CMu = [sum(CMbys(Uidx,:)); sum(CM2p(Uidx,:))];
 FM(4).all = round(100*CMu/sum(CMu(1,:)),3)/100;
->>>>>>> Stashed changes
 
 %Plot all Confusion Matrices
 clims = [0.3 1];
@@ -94,22 +91,15 @@ for i = 1:4
     text(2,1,num2str(FM(i).all(1,2)),'FontSize',15);
     text(0.75,2,num2str(FM(i).all(2,1)),'FontSize',15);
     text(1.75,2,num2str(FM(i).all(2,2)),'FontSize',15);
-<<<<<<< Updated upstream
-    set(gca, 'xtick', [1:2], 'ytick', [1:2], 'fontsize', 10, 'tickdir', 'out')
-=======
     set(gca, 'xtick', [1:2], 'ytick', [1:2], 'fontsize', 10, ...
         'tickdir', 'out','xaxislocation', 'top');
->>>>>>> Stashed changes
-    set(gca,'xticklabel',{'Bayes', 'TwoP'});
-    set(gca,'yticklabel',{'Bayes', 'TwoP'});
+    set(gca,'xticklabel',{'AB', 'S+U'});
+    set(gca,'yticklabel',{'AB', 'S+U'});
     xlabel('Fitted Model');
     ylabel('Simulated Model');
     title(Conditions{i});
 end
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 end
 
